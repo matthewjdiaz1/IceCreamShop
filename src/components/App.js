8ccc81588a8f44f5fc4a0e038fc9ea14b5374282
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-09-25 12:18:53 -0700

    added styles to src

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import IcecreamCarousel from './IcecreamCarousel';
 
-// const data = require('../../data.js');
 const data = require('../data.js');
 
 class App extends React.Component {
