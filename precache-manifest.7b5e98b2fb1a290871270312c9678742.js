1a081090c4a883cc0b28c2474d1618e37a403157
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-09-25 11:55:02 -0700

    Updates

---
+++ precache-manifest.7b5e98b2fb1a290871270312c9678742.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb1eb82ebaf1805d609c6ab36df409e",
+    "url": "/IceCreamShop/index.html"
+  },
+  {
+    "revision": "5dac35ef73c55a9ab26d",
+    "url": "/IceCreamShop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6bba21fd3d990f5bcc85",
+    "url": "/IceCreamShop/static/js/2.ebe9c038.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IceCreamShop/static/js/2.ebe9c038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dac35ef73c55a9ab26d",
+    "url": "/IceCreamShop/static/js/main.b3af5dac.chunk.js"
+  },
+  {
+    "revision": "da5d9eada8499c48f725",
+    "url": "/IceCreamShop/static/js/runtime-main.77b336d7.js"
+  },
+  {
+    "revision": "6f00992deac15060ed46a19b9234d425",
+    "url": "/IceCreamShop/static/media/Chocolate.6f00992d.png"
+  },
+  {
+    "revision": "dd884e1419f1fde46e7edd4457566776",
+    "url": "/IceCreamShop/static/media/Raspberry.dd884e14.png"
+  },
+  {
+    "revision": "60ccf4efdaddd731a41b86b57c001e62",
+    "url": "/IceCreamShop/static/media/yellow.60ccf4ef.png"
+  }
+]);
