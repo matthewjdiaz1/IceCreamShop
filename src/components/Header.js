5ccd3da4c8109e7c6a1100a18673006dc4d96345
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-09-28 13:30:35 -0700

    added button hyperlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ class Header extends React.Component {
           <HeaderButton text={'Shops'} hoverColor={this.props.secondaryColor} link={'https://www.google.com/search?hl=en&sxsrf=ALeKk02uVvjSBuoLK9KtEvpWSksTiQUyLg:1601323295241&q=sf+ice+cream&npsic=0&rflfq=1&rldoc=1&rlha=0&rllag=37775784,-122428213,1662&tbm=lcl&sa=X&ved=2ahUKEwjNsruR0ozsAhUEJTQIHXijA2UQjGp6BAgMEFE&biw=1440&bih=797#rlfi=hd:;si:;mv:[[37.8024147,-122.387529],[37.7392725,-122.5039679]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!1m4!1u22!2m2!21m1!1e1!2m1!1e2!2m1!1e1!2m1!1e3!3sIAE,lf:1,lf_ui:9'} />
           <HeaderButton text={'About Us'} hoverColor={this.props.secondaryColor} link={'https://matthewjdiaz1.github.io/Profile'} />
           <HeaderButton text={'Locator'} hoverColor={this.props.secondaryColor} />
-          <HeaderButton text={'Recipes'} hoverColor={this.props.secondaryColor} link={'https://www.google.com/search?q=icecream+recepes&oq=icecream+recepes&aqs=chrome..69i57j0l7.3380j0j4&sourceid=chrome&ie=UTF-8'} />
+          <HeaderButton text={'Recipes'} hoverColor={this.props.secondaryColor} link={'https://www.google.com/search?q=ice+cream+recipes&oq=ice+cream+recipes&aqs=chrome..69i57j0l5j69i60l2.3441j0j9&sourceid=chrome&ie=UTF-8'} />
         </div>
         <div
           className="header-container-right"
--- src/components/LearnMore.js
+++ src/components/LearnMore.js
@@ -3,7 +3,13 @@ import React from 'react';
 class LearnMore extends React.Component {
   render() {
     return (
-      <div className="icecream-learn-more" style={{ backgroundColor: this.props.hoverColor }}>
+      <div
+        className="icecream-learn-more"
+        style={{ backgroundColor: this.props.hoverColor }}
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = 'https://www.google.com/search?q=here%27s+where+you+would+learn+more+about+ice+cream&oq=here%27s+where+you+would+learn+more+about+ice+cream&aqs=chrome..69i57j33.17682j0j7&sourceid=chrome&ie=UTF-8';
+        }}>
         Learn More
       </div>
     );
